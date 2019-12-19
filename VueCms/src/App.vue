34e10394c2a4c3a77c8607e657b354e2c032c2c0
bkwym <759184489@qq.com>
2019-12-19 10:27:00 +0800

    1

--- VueCms/src/App.vue
+++ VueCms/src/App.vue
@@ -20,7 +20,7 @@
                     <span class="mui-tab-label">会员</span>
                 </a>
                 <a class="mui-tab-item" href="#tabbar-with-contact">
-                    <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">0</span></span>
+                    <span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">1</span></span>
                     <span class="mui-tab-label">购物车</span>
                 </a>
                 <a class="mui-tab-item" href="#tabbar-with-map">
