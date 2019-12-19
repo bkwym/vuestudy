970f21707ef6ed969dbce59e17470c6e9f8e745c
bkwym <759184489@qq.com>
2019-12-19 17:35:25 +0800

    Vue tabbar 布局设计

--- VueCms/src/main.js
+++ VueCms/src/main.js
@@ -3,6 +3,15 @@
 
 // console.log('项目开始了');
 import Vue from 'vue'
+
+//加载路由组件 
+import VueRouter from  'vue-router'
+//注册路由
+Vue.use(VueRouter)
+//导入路由组件 
+import router from './Router.js'
+
+
 import app from './App.vue'
 
 //导入mui样式
@@ -19,6 +28,7 @@ Vue.component(Header.name,Header)
 var vm=new Vue({
     el:"#app",
     data:{},
-    render:c=>c(app)
+    render:c=>c(app),
+    router//挂载路由
 })
 
